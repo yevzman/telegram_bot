d97217cc29a5e1951510d84d78855c5d11e63771
Evzman Yan <yalevzman@edu.hse.ru>
2021-12-24 06:11:18 +0300

    add workflow to repository which check each push request using pylint

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+"""Телеграм бот который умеет добавлять в хранилище и возвращать из него случайный мем"""
 import os
 import random
 import telebot
@@ -8,21 +9,28 @@ bot_ = telebot.TeleBot(token)
 
 @bot_.message_handler(commands=['start'], content_types=['text'])
 def start(message):
-    text = 'Добро пожаловать в бот рандомных мемасиков!\nЧтобы получить мем введите команду /get\nЕсли хотите добавить новый мем в базу данных, просто пришлите фото'
+    """Хэндлер приветствия пользователя"""
+    text = 'Добро пожаловать в бот рандомных мемасиков!\n' \
+           'Чтобы получить мем введите команду /get\n' \
+           'Если хотите добавить новый мем в базу данных, просто пришлите фото'
     bot_.send_message(message.from_user.id, text)
 
 
 @bot_.message_handler(commands=['get'], content_types=['text'])
 def get_mem_handler(message):
+    """Хэндлер который обрабатывает запрос /get возвращающий случайный мем"""
     print('Processing /get request')
     random_num = random.randint(0, len(os.listdir('memes')) - 1)
-    bot_.send_photo(message.from_user.id, photo=open('memes/mem{}.jpg'.format(random_num), 'rb'))
+    with open(f'memes/mem{random_num}.jpg', 'rb') as send_photo:
+        bot_.send_photo(message.from_user.id, photo=send_photo)
 
 
 @bot_.message_handler(content_types=['photo'])
 def add_mem_handler(message):
+    """Хэндлер который добавляет фото присланное пользователем в хранилище"""
     print('Processing \"add new mem\" request')
-    path = 'memes/mem{}.jpg'.format(len(os.listdir('memes')))
+    index = len(os.listdir('memes'))
+    path = f'memes/mem{index}.jpg'
     file_id = message.photo[2].file_id
     file_info = bot_.get_file(file_id)
     downloaded_file = bot_.download_file(file_info.file_path)
@@ -31,6 +39,7 @@ def add_mem_handler(message):
 
 
 def main():
+    """Начало работы бота"""
     random.seed(2341)
     print('Starting...................')
     bot_.polling(none_stop=True, interval=0)
